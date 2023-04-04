d8614b415c8cfd14ed57c6d1fbb0190709e8fbc3
Efrain <legjin9966@gmail.com>
2023-04-04 19:51:11 -0400

    Tenancyforlaravel Fresh Install - QuickStart

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+// App\Models\Tenant::all()->runForEach(function () {
+//     App\Models\User::factory()->create();
+// });
+
+Route::middleware([
+    'web',
+    InitializeTenancyByDomain::class,
+    PreventAccessFromCentralDomains::class,
+])->group(function () {
+    Route::get('/', function () {
+        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+    });
+});
+
+Route::get('/', function () {
+    dd(\App\Models\User::all());
+    return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+});
+
